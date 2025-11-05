d3f6b2a3f3bdcc195a45f8abc82f49fb3f77f9f7
IDRISS <idrisswater0@gmail.com>
2025-11-05 11:12:02 +0100

    ajout de la fonctionnalité delete complète

--- src/main/java/ma/mundiapolis/mundiapolisspringmvc/web/ProductController.java
+++ src/main/java/ma/mundiapolis/mundiapolisspringmvc/web/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,10 +14,16 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-@GetMapping("/index")
-public String index(Model model){
-    List<Product> products = productRepository.findAll();
-    model.addAttribute("productList",products);
-    return "products";
-}
+    @GetMapping("/index")
+    public String index(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productList", products);
+        return "products";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") Long id) {
+        productRepository.deleteById(id);
+        return "redirect:/index";
+    }
 }
